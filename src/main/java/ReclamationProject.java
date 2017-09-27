8e570b42df6c992284852b4c5083053cc2b45b23
Felix Hanau <felixhanau@wirelessprv-10-195-99-224.near.illinois.edu>
2017-09-27 14:24:17 -0500

    Blah Blah Blah

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -6,21 +6,43 @@
  * 3. Improve the name of the methods and variables
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
+ *
+ * @author  me
  */
+public class ReclamationProject {
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+    /*
+     * Sleeps. A lot.
+     * @param a does work.
+     * @param b also does work.
+     */
+    static String doit(final String aa, final String bb) {
+        String a = aa;
+        String b = bb;
+        if (a.length() > b.length()) {
+            String temp = a;
+            a = b;
+            b = temp;
+        }
+        String r = "";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                        r = a.substring(i, i + j);
+                    }
+                }
+            } // Ah yeah
         } return r; }
+
+    /**
+     * Sleeps. A lot. Too.
+     * @param args Ignored.
+     */
+    public static void main(final String[] args) {
+
+    }
 }
